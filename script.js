c26aec22a6ea7bf1e19989323121afa1f51b1844
rafal A <rafalandrzejczuk1@gmail.com>
2023-06-09 16:15:17 +0200

    commit2

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 
 
-    let addDataContainer= document.getElementById("post")
-let counter= 1; //poczatek licznika.
-fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=10')
-    .then(response => response.json())
-    .then((data) => {
-        console.log(data);
-        data.forEach((objekt) => {
-            console.log(objekt.thumbnailUrl);
 
-            let thumbnailUrl = document.createElement('img');
-            let title = document.createElement('p')
-            title.innerHTML = counter + '. ' + objekt.title; //dodajemy licznik 
-            thumbnailUrl.src = objekt.thumbnailUrl;
-            
+let page = 0
 
-        
-            addDataContainer.appendChild(thumbnailUrl);
-            addDataContainer.appendChild(title);
 
-            counter++; //zwieksza wartosc licznka dla kazdego elementu
+const getData = () => {
+
+
+    let addDataContainer = document.getElementById("post")
+    // let counter = 1; //poczatek licznika.
+    fetch(`https://jsonplaceholder.typicode.com/photos?_start=${page}&_limit=10`)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data);
+            data.forEach((objekt) => {
+                console.log(objekt.thumbnailUrl);
+
+                let thumbnailUrl = document.createElement('img');
+                let title = document.createElement('p')
+                title.innerHTML = objekt.id + '. ' + objekt.title; //dodajemy licznik 
+                thumbnailUrl.src = objekt.thumbnailUrl;
+
+
+
+                addDataContainer.appendChild(thumbnailUrl);
+                addDataContainer.appendChild(title);
+
+                // counter++; //zwieksza wartosc licznka dla kazdego elementu
+            });
+
+             page=page+10  //wartosc 
+
         });
 
+    }
+
+   let  button = document.getElementById('btn')
+
+   button.addEventListener('click',getData)
+    
+ 
 
 
-    });
-
-
-
-    const scrollToEndOfPage = ()=>{
+    getData()
+    const scrollToEndOfPage = () => {
         console.log('scrollToEndOfPage()')
     }
